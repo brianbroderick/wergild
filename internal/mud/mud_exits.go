8a712162fe34636de8bcf0bec5253883237cde0c
Brian Broderick <brian.broderick@podium.com>
2020-07-09 16:48:53 -0600

    cleaning up nested structs

--- internal/mud/mud_exits.go
+++ internal/mud/mud_exits.go
@@ -4,7 +4,7 @@ func NewExit(uid string, dir string) Exit {
 	tok := Lookup(dir)
 
 	return Exit{
-		Dest:      []Room{Room{UID: uid}},
+		Dest:      []Room{{UID: uid}},
 		Direction: tok.String(),
 		Portal:    "OPEN",
 		Type:      "Exit",
