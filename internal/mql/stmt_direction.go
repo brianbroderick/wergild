4accc1d9b159c54ffa4c7991beeea61f1c4caae7
Brian Broderick <brian.broderick@podium.com>
2020-07-25 18:34:35 -0600

    remove log msg

--- internal/mql/stmt_direction.go
+++ internal/mql/stmt_direction.go
@@ -2,8 +2,6 @@ package mql
 
 import (
 	"bytes"
-
-	"github.com/brianbroderick/logit"
 )
 
 // DirectionStatement represents a command for looking at a room or object.
@@ -17,9 +15,6 @@ func (p *Parser) parseDirectionStatement() (*DirectionStatement, error) {
 	p.Unscan()
 	stmt := &DirectionStatement{}
 	stmt.Token, _, _ = p.ScanIgnoreWhitespace()
-
-	logit.Info(Tokens[stmt.Token])
-
 	return stmt, nil
 }
 
