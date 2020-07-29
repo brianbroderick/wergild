961a68065026c8da94a46f58c862664df5b79829
Brian Broderick <brian.broderick@podium.com>
2020-07-28 22:23:16 -0600

    simplify world

--- cmd/seed/mobs.go
+++ cmd/seed/mobs.go
@@ -6,15 +6,6 @@ import (
 )
 
 func (world *worldSeed) setMobs() {
-	erik := mud.NewMob()
-	erik.UID = "_:erik"
-	erik.Name = "Erik"
-	erik.Slug = "Erik"
-	erik.Title = "the apprentice smith"
-	erik.Desc = "This tall lad already shows the stature of his uncle in sheer height. Bulging muscles have started to fill out his gangly frame, and his skin has taken on the dark hue of blacksmiths everywhere. His dark hair is clipped into a short burr, showing a faded scar near his temple. He has kind, merry brown eyes and you sense that he may have a soft spot for those new to this world."
-
-	world.Mobs = append(world.Mobs, erik)
-
 	azkul := mud.NewMob()
 	azkul.UID = "_:azkul"
 	azkul.Name = "Azkul"
